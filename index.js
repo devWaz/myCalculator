593994f9970b9b4599afbb1532b9af49d18e6097
devWaz <fawazodessy@gmail.com>
2022-04-17 23:34:13 +0100

    calculator

---
+++ index.js
@@ -0,0 +1,41 @@
+let displayText = document.getElementById("display")
+
+function numZero(){
+    displayText.textContent = 0
+}
+
+function numOne(){
+    displayText.textContent = 1
+}
+
+function numTwo(){
+    displayText.textContent = 2
+}
+
+function numThree(){
+    displayText.textContent = 3
+}
+
+function numFour(){
+    displayText.textContent = 4
+}
+
+function numFive(){
+    displayText.textContent = 5
+}
+
+function numSix(){
+    displayText.textContent = 6
+}
+
+function numSeven(){
+    displayText.textContent = 7
+}
+
+function numEight(){
+    displayText.textContent = 8
+}
+
+function numNine(){
+    displayText.textContent = 9
+}
