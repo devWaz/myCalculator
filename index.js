ba6c247183a59a8b7e28104d4abcfa9f7f7d0100
devWaz <fawazodessy@gmail.com>
2022-04-19 21:29:40 +0100

    final

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ buttons.map(button => {
         switch(a.target.innerText){
             case 'AC':
                 displayText.innerText = ' ';
+                break;
+            case '=':
+                try{
+                    displayText.innerText = eval(display.innerText);
+                }
+                catch{
+                    display.innerText = 'Error'
+                }
+                break;
+            case 'DEL' :
+                if (display.innerText){
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+                default: display.innerText += a.target.innerText;
         }
-    }
-    )
-}
-
-
-)
+    });
+});
