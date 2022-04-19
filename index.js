216ea10b23e4e4aabb91a8c5398d46c701d9f10c
devWaz <fawazodessy@gmail.com>
2022-04-19 21:04:11 +0100

    new js code

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 let displayText = document.getElementById("display")
+let buttons = Array.from(document.getElementsByClassName('button'))
 
+innerText = 0;
+
+buttons.map(button => {
+    button.addEventListener('click', (a) =>{
+        switch(a.target.innerText){
+            case 'AC':
+                displayText.innerText = ' ';
+        }
+    }
+    )
+}
+
+
+)
